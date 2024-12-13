cdbed75f792ffe7cca395d49256185b909e07523
Gabriele Sbaiz <gabriele.sbaiz@2601.it>
2024-12-13 09:49:54 +0100

    Multiple maps on same resource even inside tabls

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,23 @@
-let mix = require('laravel-mix')
+const path = require("path");
 
-require('./nova.mix')
+let mix = require("laravel-mix");
+
+require("./nova.mix");
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/field.js', 'js')
+  .setPublicPath("dist")
+  .js("resources/js/field.js", "js")
   .vue({ version: 3 })
-  .css('resources/css/field.css', 'css')
-  .nova('imumz/nova-4-field-map')
+  .css("resources/css/field.css", "css")
+  .nova("imumz/nova-4-field-map");
+
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      "laravel-nova": path.resolve(
+        __dirname,
+        "vendor/laravel/nova/resources/js/mixins/packages.js"
+      ),
+    },
+  },
+});
